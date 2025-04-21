1d7904ee84d269870ef8ea3c7c91f903e47d77c2
Ekaterina <burlutskayaekaterinka@yandex.ru>
2025-04-22 00:20:14 +0500

    It's finally alive

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -5,19 +5,19 @@ using UnityEngine;
 public class Movement : MonoBehaviour
 {
     private Vector3 _savedPosition;
-    //private Vector3 _offset = new Vector3(0.01f,0);
-    //область видимости метода Апдейта, глобальная переменная
-    void Start()
-    {
-        
-    }
+    private Vector3 _offsetRight = new Vector3(0.01f, 0); 
+    private Vector3 _offsetLeft = new Vector3(-0.01f, 0); //=-_offsetRight
+    private Vector3 _offsetUp = new Vector3(0,0.01f); 
+    private Vector3 _offsetDown = new Vector3(0,-0.01f); //=-_offsetUp
+    //глобальная переменная
     void Update()
     {
         var transformCube = GetComponent<Transform>();
-        _savedPosition = transformCube.position + new Vector3(0.01f,0);
-        transformCube.position = _savedPosition;
+        //_savedPosition = transformCube.position + new Vector3(0.01f,0);
+        //transformCube.position = _savedPosition;
+        _savedPosition = transformCube.position;
 
-        if (transformCube.position.x > 8 && transformCube.position.y < -4)
+        if (transformCube.position.x < 8)
         {
             transformCube.position = _savedPosition + new Vector3(0.01f, 0); //нужно убрать изменение по оси x
         }
