9cafd98d0d080fc3d56ffec4f17c9cb875f8f933
Ekaterina <burlutskayaekaterinka@yandex.ru>
2025-04-22 00:58:30 +0500

    It's working 1
    ну это финалочка

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -17,7 +17,7 @@ public class Movement : MonoBehaviour
         var transformCube = GetComponent<Transform>();
         _savedPosition = transformCube.position;
 
-        if (transformCube.position.x <= 8 & transformCube.position.y >= -4)
+        if (transformCube.position.x <= 8 & transformCube.position.y <= -4)
         {
             transformCube.position = _savedPosition + _offsetRight;
         }
@@ -25,11 +25,11 @@ public class Movement : MonoBehaviour
         {
             transformCube.position = _savedPosition + _offsetUp;
         }
-        else if (transformCube.position.x >= 8 & transformCube.position.y >= 4)
+        else if (transformCube.position.x >= -8 & transformCube.position.y >= 4)
         {
             transformCube.position = _savedPosition + _offsetLeft;
         }
-        else if (transformCube.position.y < -4)
+        else if (transform.position.x <= -8 & transformCube.position.y >= -4)
         {
             transformCube.position = _savedPosition + _offsetDown;
         }
