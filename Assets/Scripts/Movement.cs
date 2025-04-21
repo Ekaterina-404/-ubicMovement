75be47597e54ad8cb38568a5075b0ea3a6f9dee0
Ekaterina <burlutskayaekaterinka@yandex.ru>
2025-04-22 00:58:30 +0500

    It's almost working

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,29 +1,37 @@
 using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
     private Vector3 _savedPosition;
-    private Vector3 _offsetRight = new Vector3(0.01f, 0); 
+    private Vector3 _offsetRight = new Vector3(0.01f, 0);
     private Vector3 _offsetLeft = new Vector3(-0.01f, 0); //=-_offsetRight
-    private Vector3 _offsetUp = new Vector3(0,0.01f); 
-    private Vector3 _offsetDown = new Vector3(0,-0.01f); //=-_offsetUp
+    private Vector3 _offsetUp = new Vector3(0, 0.01f);
+    private Vector3 _offsetDown = new Vector3(0, -0.01f); //=-_offsetUp
+
     //глобальная переменная
     void Update()
     {
         var transformCube = GetComponent<Transform>();
-        //_savedPosition = transformCube.position + new Vector3(0.01f,0);
-        //transformCube.position = _savedPosition;
         _savedPosition = transformCube.position;
 
-        if (transformCube.position.x < 8)
+        if (transformCube.position.x <= 8 & transformCube.position.y >= -4)
         {
-            transformCube.position = _savedPosition + new Vector3(0.01f, 0); //нужно убрать изменение по оси x
+            transformCube.position = _savedPosition + _offsetRight;
         }
-        else if (transformCube.position.x > 8)
+        else if (transformCube.position.x >= 8 & transformCube.position.y <= 4)
         {
-            transformCube.position = _savedPosition + new Vector3(0, 0.01f);
+            transformCube.position = _savedPosition + _offsetUp;
+        }
+        else if (transformCube.position.x >= 8 & transformCube.position.y >= 4)
+        {
+            transformCube.position = _savedPosition + _offsetLeft;
+        }
+        else if (transformCube.position.y < -4)
+        {
+            transformCube.position = _savedPosition + _offsetDown;
         }
     }
 }
