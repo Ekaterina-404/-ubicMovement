bf7cfcb6990dee0caabbf1cc16500c58caaf9ae7
Ekaterina <burlutskayaekaterinka@yandex.ru>
2025-05-03 22:08:35 +0500

    The up-left diagonal works, but the up-right diagonal doesn't.

--- Assets/Scripts/Diagonal Movement.cs
+++ Assets/Scripts/Diagonal Movement.cs
@@ -6,25 +6,45 @@ using UnityEngine;
 public class DiagonalMovement : MonoBehaviour
 {
     private Vector3 _savedPosition;
-    private Vector3 _offsetRight = new Vector3(0.01f, 0);
-    private Vector3 _offsetLeft = new Vector3(-0.01f, 0); //=-_offsetRight
-    private Vector3 _offsetUp = new Vector3(0, 0.01f);
-    private Vector3 _offsetDown = new Vector3(0, -0.01f); //=-_offsetUp
-    private Vector3 _offsetDiagonalUpLeft = new Vector3(-0.01f, 0.01f);
+    
+    private Vector3 _offsetRight = new Vector3(0.03f, 0);
+    private Vector3 _offsetLeft = new Vector3(-0.03f, 0); //=-_offsetRight
+    private Vector3 _offsetUp = new Vector3(0, 0.03f);
+    private Vector3 _offsetDown = new Vector3(0, -0.03f); //=-_offsetUp
+    private Vector3 _offsetDiagonalUpLeft = new Vector3(-0.03f, 0.03f);
+    private Vector3 _offsetDiagonalUpRight = new Vector3(0.03f, 0.03f);
 
-    //глобальная переменная
     void Update()
     {
         var transformCube = GetComponent<Transform>();
         _savedPosition = transformCube.position;
 
-        if (transformCube.position.x <= 8) // норм
+        if (transformCube.position.x <= 8 && transformCube.position.y <= -4) //по границе, рабочая
         {
-            transformCube.position = _savedPosition + _offsetRight;
+            transformCube.position = _savedPosition + new Vector3(0.03f, 0);
         }
-        else if (transformCube.position.x >= 8)
+        else if (transformCube.position.x >= -8 && transformCube.position.y >= -4) //диагональ чееткая, рабочая
         {
-            transformCube.position = _savedPosition + (-_offsetRight) + _offsetUp;
+            transformCube.position = _savedPosition + new Vector3(-0.03f, 0.015f);
         }
+        else if (transformCube.position.x <= -8 && transformCube.position.y >= -4) //по границе
+        {
+            transformCube.position = _savedPosition + new Vector3(0, -0.03f);
+        }
+        else if (transform.position.x <= -8 && transform.position.y <= -4) //диагональ, не работает
+        {
+            transformCube.position = _savedPosition + new Vector3(0.03f, 0.015f);
+        }
+
+        /*
+        else if (transform.position.x >= 8) //по границе
+        {
+            transformCube.position = _savedPosition + _offsetLeft;
+        }
+       else if (transform.position.x <= -8) //по границе
+        {
+            transformCube.position = _savedPosition + _offsetLeft;
+        }
+        */
     }
 }
