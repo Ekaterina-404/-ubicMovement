e2f71fa667b1862624d948d8e6f69f7ceb8ecf67
Ekaterina <burlutskayaekaterinka@yandex.ru>
2025-05-07 01:11:53 +0500

    The beginning of the linear interpolation method
    Does not work

--- Assets/Scripts/Diagonal Movement.cs
+++ Assets/Scripts/Diagonal Movement.cs
@@ -7,50 +7,43 @@ using UnityEngine;
 public class DiagonalMovement : MonoBehaviour
 {
     private Vector3 _savedPosition;
-    private bool _isFirstStage;
-    private bool _isSecondStage;
+    private Vector3 _firstPoint;
+    [SerializeField] private Vector3[] _points = new Vector3[4];
 
-    private Vector3 _offsetRight = new Vector3(0.03f, 0);
-    private Vector3 _offsetLeft = new Vector3(-0.03f, 0); //=-_offsetRight
-    private Vector3 _offsetUp = new Vector3(0, 0.03f);
-    private Vector3 _offsetDown = new Vector3(0, -0.03f); //=-_offsetUp
-    private Vector3 _offsetDiagonalUpLeft = new Vector3(-0.03f, 0.03f);
-    private Vector3 _offsetDiagonalUpRight = new Vector3(0.03f, 0.03f);
+    //private Vector3 _offsetRight = new Vector3(0.03f, 0);
+    //private Vector3 _offsetLeft = new Vector3(-0.03f, 0); //=-_offsetRight
+    //private Vector3 _offsetUp = new Vector3(0, 0.03f);
+    //private Vector3 _offsetDown = new Vector3(0, -0.03f); //=-_offsetUp
+    //private Vector3 _offsetDiagonalUpLeft = new Vector3(-0.03f, 0.03f);
+    //private Vector3 _offsetDiagonalUpRight = new Vector3(0.03f, 0.03f);
 
     private void Start()
     {
-        _isFirstStage = true;
     }
 
     void Update()
     {
         var transformCube = GetComponent<Transform>();
         _savedPosition = transformCube.position;
+    }
 
-        if (_isFirstStage)
+    private void MoveToWithLerp(Vector3[] positions, float time)
+    {
+        for (int i = 0; i < positions.Length; i++)
         {
-            if (transformCube.position.x <= 8 && transformCube.position.y <= -4) //по границе, рабочая
+            _firstPoint = transform.position;
+            float t = 0f;
+
+            while (t < 1)
             {
-                Debug.Log(1);
-                transformCube.position = _savedPosition + new Vector3(0.03f, 0);
+                t += Time.deltaTime / time;
+                transform.position = Vector3.Lerp(_firstPoint, positions[i], t);
+                //yield return null;
             }
-            else if (transformCube.position.x >= -8 && transformCube.position.y >= -4) //диагональ чееткая, рабочая
-            {
-                Debug.Log(2);
-                transformCube.position = _savedPosition + new Vector3(-0.03f, 0.015f);
-            }
-            else if (transformCube.position.x <= -8 && transformCube.position.y >= -4) //по границе
-            {
-                Debug.Log(3);
-                transformCube.position = _savedPosition + new Vector3(0, -0.03f);
-                
-                
-            }
-        }
-        else if (_isSecondStage)
-        {
-            
-            
+
+            transform.position = positions[i];
         }
     }
 }
+        
+   
