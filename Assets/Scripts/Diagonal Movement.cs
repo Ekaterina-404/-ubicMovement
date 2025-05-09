ce6e5972479ffabd9eb0ecc934361db24cf9be31
Ekaterina <burlutskayaekaterinka@yandex.ru>
2025-05-10 00:44:30 +0500

    The linear interpolation command is working

--- Assets/Scripts/Diagonal Movement.cs
+++ Assets/Scripts/Diagonal Movement.cs
@@ -6,15 +6,21 @@ using UnityEngine;
 
 public class DiagonalMovement : MonoBehaviour
 {
-    private static Vector3 _firstPoint;
-    [SerializeField] private Vector3[] _points = new Vector3[9];
-    private static Vector3 _savedPosition;
+    //[SerializeField] private Vector3[] _points = new Vector3[9];
+    //private static Vector3 _savedPosition;
+    //private float speed = 2f;
+    private static Vector3 _firstPosition = new Vector3(-8, -4);
+    private static Vector3 _secondPosition = new Vector3(8, 4);
+    float timeElapsed;
+    float lerpDuration = 3;
+
+    //float lerpedValue;
 
     void Start()
     {
         var transformCube = GetComponent<Transform>();
-        _savedPosition = transformCube.position;
-        _firstPoint = _savedPosition;
+        transformCube.position = _firstPosition;
+        //_savedPosition = transformCube.position;
     }
 
     void Update()
@@ -22,23 +28,14 @@ public class DiagonalMovement : MonoBehaviour
         //MoveToWithLerp(_points, t);
         //private void MoveToWithLerp(Vector3[] positions, float time)
 
-        float t = 0f;
-        for (int i = 0; i < _points.Length; i++)
-        {
-            if (i == 0)
-            {
-                while (t < 1)
-                {
-                    t += Time.deltaTime / t;
-                    yield return null;
-                }
-                
-                var thisTransformCube = Vector3.Lerp(_firstPoint, _points[i], t);
-                _savedPosition = thisTransformCube;
-            }
+        //for (int i = 0; i < _points.Length; i++)
+        //if (i == 0)
 
-            //transform.position = positions[i];
-            //_firstPoint = transform.position;
+
+        if (timeElapsed < lerpDuration)
+        {
+            transform.position = Vector3.Lerp(_firstPosition, _secondPosition, timeElapsed / lerpDuration);
+            timeElapsed += Time.deltaTime;
         }
     }
 }
